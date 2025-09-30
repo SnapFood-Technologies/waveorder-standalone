95911ebf4695c916316ed830e512b902b3fa1e68
Griseld <development@venueboost.io>
2025-09-30 23:06:24 +0300

    basic form

---
+++ src/app/superadmin/businesses/new/page.tsx
@@ -0,0 +1,6 @@
+// app/superadmin/businesses/new/page.tsx
+import { CreateBusinessForm } from '@/components/superadmin/CreateBusinessForm'
+
+export default function NewBusinessPage() {
+  return <CreateBusinessForm />
+}
