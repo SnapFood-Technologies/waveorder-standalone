7161f5fc123fefc48d4c1e6b12464c8600be7bb6
Griseld <development@venueboost.io>
2025-10-24 20:56:52 +0300

    fix

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1812,6 +1812,7 @@ export async function sendSupportMessageReceivedEmail({
   }
 }
 
+// @ts-ignore - feat: complete help and support and notification module
 export async function sendSupportTicketCommentEmail({
   to,
   recipientName,
